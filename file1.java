e578cd2a6ba419e180212c9e158fc04a2625fea5
SyedBaaqui <syedmohammadbaaqui786@gmail.com>
2024-05-25 10:04:20 +0530

    One while loop is added

--- file1.java
+++ file1.java
@@ -3,4 +3,10 @@ import java.util.*;
 public static void main(String args[])
 {
 	System.out.println("Syed Moahmmad Baaqui");
+	for(int i=0; i<=5;i++)
+	{	while(i%2==0)
+		{
+			System.out.println(i);
+		}
+	}
 }

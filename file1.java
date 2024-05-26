838d715902661cd03201e8242c25580c0f90ab64
SyedBaaqui <syedmohammadbaaqui786@gmail.com>
2024-05-26 10:45:28 +0530

    for loop removed

--- file1.java
+++ file1.java
@@ -3,7 +3,7 @@ import java.util.*;
 public static void main(String args[])
 {
 	System.out.println("Syed Moahmmad Baaqui");
-	for(int i=0; i<=5;i++)
+	//for(int i=0; i<=5;i++)
 	{	while(i%2==0)
 		{
 			System.out.println(i);
